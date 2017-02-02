d4a17b50a342db5ba9d0ff7e3c523dbcaa9604e8
YudizPayal <payal@itindia.co.in>
2017-02-02 17:15:27 +0530

    my first commit

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="6781d306-32ba-41dd-af55-fbf3cdeeca0f", modulePackage="com.firebaseplayer", sdkRoot="/Users/Yudiz/Documents/Payal/Extra/android-sdks-new",layoutInfoDir="/Users/Yudiz/Documents/Payal/AllProjects/extra/projects/FireBaseStoragedemo/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=15,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
---
+++ app/src/main/java/com/firebaseplayer/model/ModelVideoItem.java
@@ -0,0 +1,11 @@
+package com.firebaseplayer.model;
+
+public class ModelVideoItem {
+    public int imageResId;
+    public String titleResId;
+
+    public ModelVideoItem(int imageResId, String titleResId){
+        this.imageResId = imageResId;
+        this.titleResId = titleResId;
+    }
+}
